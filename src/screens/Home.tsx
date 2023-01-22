6687dcc0f47f96733d0dc246c12d832549239bd4
EddyPBR <edvaldojunior1310@gmail.com>
2023-01-22 11:29:39 -0300

    :sparkles: Home screen was integrated with api

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,22 @@
+import { useEffect, useState } from "react";
 import { View, Text, ScrollView } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import dayjs from "dayjs";
+
+import { getSummary } from "../api/getSummary";
 
 import { generateDatesFromYearBeginning } from "../utils/generate-range-between-dates";
 
 import { Header } from "../components/Header";
 import { DAY_SIZE, HabitDay } from "../components/HabitDay";
-import { useNavigation } from "@react-navigation/native";
+import { Loading } from "../components/Loading";
+
+interface ISummary {
+  id: string;
+  date: string;
+  amount: number;
+  completed: number;
+}
 
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
 const datesFromYearBeginning = generateDatesFromYearBeginning();
@@ -14,6 +26,31 @@ const amountOfDaysToFill =
 
 export function Home() {
   const { navigate } = useNavigation();
+
+  const [summary, setSummary] = useState<ISummary[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  async function loadSummary() {
+    try {
+      setIsLoading(true);
+      const response = await getSummary();
+      setSummary(response.data);
+    } catch (error: any) {
+      alert(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    return () => {
+      loadSummary();
+    };
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
 
   return (
     <View className="flex-1 bg-background px-8 pt-16">
@@ -36,12 +73,24 @@ export function Home() {
         </View>
 
         <View className="flex-row flex-wrap">
-          {datesFromYearBeginning.map((date) => (
-            <HabitDay
-              key={date.toISOString()}
-              onPress={() => navigate("habit", { date: date.toISOString() })}
-            />
-          ))}
+          {datesFromYearBeginning.map((date) => {
+            const dayWithHabits = summary.find((day) => {
+              return dayjs(date).isSame(day.date, "day");
+            });
+
+            const amountOfHabits = dayWithHabits?.amount || 0;
+            const amountCompleted = dayWithHabits?.completed || 0;
+
+            return (
+              <HabitDay
+                key={date.toISOString()}
+                date={date}
+                amountOfHabits={amountOfHabits}
+                amountCompleted={amountCompleted}
+                onPress={() => navigate("habit", { date: date.toISOString() })}
+              />
+            );
+          })}
 
           {amountOfDaysToFill > 0 &&
             Array.from({ length: amountOfDaysToFill }).map((_, index) => (
