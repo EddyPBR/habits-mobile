912ce6fc1ccaf4de18243236f10fa4b9691f1a41
EddyPBR <edvaldojunior1310@gmail.com>
2023-01-22 23:02:36 -0300

    :zap: adjusting HabitDay component colors

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -38,22 +38,28 @@ export function HabitDay({
 
   return (
     <TouchableOpacity
-      className={clsx("rounded-lg border-2 m-1", {
-        "bg-zinc-900 border-zinc-800": amountCompleted === 0,
-        "bg-violet-900 border-violet-800":
-          amountAccomplishedPercentage > 0 && amountAccomplishedPercentage < 40,
-        "bg-violet-800 border-violet-700":
-          amountAccomplishedPercentage >= 20 &&
-          amountAccomplishedPercentage < 40,
-        "bg-violet-700 border-violet-600":
-          amountAccomplishedPercentage >= 40 &&
-          amountAccomplishedPercentage < 60,
-        "bg-violet-600 border-violet-500":
-          amountAccomplishedPercentage >= 60 &&
-          amountAccomplishedPercentage < 80,
-        "bg-violet-500 border-violet-400": amountAccomplishedPercentage >= 80,
-        "border-white border-4": isCurrentDay,
-      })}
+      className={clsx(
+        "rounded-lg border-2 m-1",
+        {
+          "bg-zinc-900 border-zinc-800": amountAccomplishedPercentage === 0,
+          "bg-violet-900 border-violet-800":
+            amountAccomplishedPercentage > 0 &&
+            amountAccomplishedPercentage < 40,
+          "bg-violet-800 border-violet-700":
+            amountAccomplishedPercentage >= 20 &&
+            amountAccomplishedPercentage < 40,
+          "bg-violet-700 border-violet-600":
+            amountAccomplishedPercentage >= 40 &&
+            amountAccomplishedPercentage < 60,
+          "bg-violet-600 border-violet-500":
+            amountAccomplishedPercentage >= 60 &&
+            amountAccomplishedPercentage < 80,
+          "bg-violet-500 border-violet-400": amountAccomplishedPercentage >= 80,
+        },
+        {
+          "border-white border-4": isCurrentDay,
+        }
+      )}
       activeOpacity={0.7}
       style={{ width: DAY_SIZE, height: DAY_SIZE }}
       {...rest}
