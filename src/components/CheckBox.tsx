87e1942505537694c0424729a588ef7f8bafbebb
EddyPBR <edvaldojunior1310@gmail.com>
2023-01-22 23:16:27 -0300

    :sparkles: add animations in progress bar and check box components

--- src/components/CheckBox.tsx
+++ src/components/CheckBox.tsx
@@ -4,6 +4,7 @@ import {
   Text,
   TouchableOpacityProps,
 } from "react-native";
+import Animated, { ZoomIn, ZoomOut } from "react-native-reanimated";
 import { Feather } from "@expo/vector-icons";
 import colors from "tailwindcss/colors";
 
@@ -20,9 +21,13 @@ export function CheckBox({ title, checked = false, ...props }: ICheckBoxProps) {
       {...props}
     >
       {checked ? (
-        <View className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center">
+        <Animated.View
+          className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center"
+          entering={ZoomIn}
+          exiting={ZoomOut}
+        >
           <Feather name="check" size={20} color={colors.white} />
-        </View>
+        </Animated.View>
       ) : (
         <View className="h-8 w-8 bg-zinc-900 rounded-lg" />
       )}
